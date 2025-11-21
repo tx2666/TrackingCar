9a6dcaf7470164393536dcff156f841ffc03db23
QYC <13695832800@163.com>
2025-11-21 22:21:00 +0800

    把测速的结果修改为外部可调用变量

--- Hardware/Encoder.h
+++ Hardware/Encoder.h
@@ -1,6 +1,9 @@
 #ifndef __ENCODER_H
 #define __ENCODER_H
 
+extern int16_t Encoder1_Count;
+extern int16_t Encoder2_Count;
+
 void Encoder_Init(void);
 int16_t Encoder1_Get(void);
 int16_t Encoder2_Get(void);
