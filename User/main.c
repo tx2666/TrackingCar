e868ab9d4217afc9762d7bbb09ad3d708e8cfced
QYC <13695832800@163.com>
2025-11-25 11:22:22 +0800

    将UI显示和按钮检测从主函数中分离，使主函数简洁

--- User/main.c
+++ User/main.c
@@ -28,6 +28,9 @@ uint8_t Serial_Out_Mode = SERIAL_OUT_MODE_MOTOR_DATA;
 
 int16_t Tar_Step = 1;
 
+void UI_Exhibit(void);
+void Key_Monitor(void);
+
 int main(void)
 {
 	Sensor_Init();
@@ -59,536 +62,559 @@ int main(void)
 	
 	while (1)
 	{
-		if (UIpos == UI_root.Num)
+		UI_Exhibit();
+		Key_Monitor();
+	}
+}
+
+void UI_Exhibit(void)
+{
+	if (UIpos == UI_root.Num)
+	{
+		UI_Show(&UI_root);
+	}
+	else if (UIpos == UI_start.Num)
+	{
+		UI_Show(&UI_start);
+	}
+	else if (UIpos == UI_PID.Num)
+	{
+		UI_Show(&UI_PID);
+		UI_Show_PID(pPID_Motor->Kp, pPID_Motor->Ki, pPID_Motor->Kd);
+		if (pPID_Motor == &PID_Motor1)
 		{
-			UI_Show(&UI_root);
+			UI_PID_Show_Motor_Num(1);
+		}
+		else if (pPID_Motor == &PID_Motor2)
+		{
+			UI_PID_Show_Motor_Num(2);
+		}
+	}
+	else if (UIpos == UI_test.Num)
+	{
+		UI_Show(&UI_test);
+
+	}
+	else if (UIpos == UI_target.Num)
+	{
+		UI_Show(&UI_target);
+		UI_Show_test_PID_Struct(&PID_Motor1, &PID_Motor2, Tar_Step);
+	}
+	else if (UIpos == UI_sensor.Num)
+	{
+		UI_Show(&UI_sensor);
+		UI_Show_Sensor(Sensor_Data_Bit);
+	}
+	else if (UIpos == UI_serial.Num)
+	{
+		UI_Show(&UI_serial);
+		UI_Show_Serial(Serial_Out_Mode);
+	}	
+}
+
+void Key_Monitor(void)
+{
+	/* 按钮检测 */
+	/* 上 */
+	/* 单击 */
+	if (Key_GetState(KEY_UP, KEY_PRESS))
+	{
+		if 		(UIpos == UI_root.Num)
+		{
+			UI_MoveUp_Cursor(&UI_root);
 		}
 		else if (UIpos == UI_start.Num)
 		{
-			UI_Show(&UI_start);
+			UI_MoveUp_Cursor(&UI_start);
 		}
 		else if (UIpos == UI_PID.Num)
 		{
-			UI_Show(&UI_PID);
-			UI_Show_PID(pPID_Motor->Kp, pPID_Motor->Ki, pPID_Motor->Kd);
-			if (pPID_Motor == &PID_Motor1)
+			if (Edit_Mode == 1)
 			{
-				UI_PID_Show_Motor_Num(1);
+				if (UI_PID.cursor == 2)
+				{
+					pPID_Motor->Kp += 0.01;
+				}
+				else if (UI_PID.cursor == 3)
+				{
+					pPID_Motor->Ki += 0.01;
+				}
+				else if (UI_PID.cursor == 4)
+				{
+					pPID_Motor->Kd += 0.01;
+				}
 			}
-			else if (pPID_Motor == &PID_Motor2)
+			else if (Edit_Mode == 0)
 			{
-				UI_PID_Show_Motor_Num(2);
+				UI_MoveUp_Cursor(&UI_PID);
 			}
 		}
 		else if (UIpos == UI_test.Num)
 		{
-			UI_Show(&UI_test);
+			if (Edit_Mode == 1)
+			{
+				pPID_Motor->Target += 1;
+			}
+			else if (Edit_Mode == 0)
+			{
+				UI_MoveUp_Cursor(&UI_test);
+			}
+		}
+		else if (UIpos == UI_target.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_target.cursor == 2)
+				{
+					PID_Motor1.Target += Tar_Step;
+				}
+				else if (UI_target.cursor == 3)
+				{
+					PID_Motor2.Target += Tar_Step;
+				}
+				else if (UI_target.cursor == 4)
+				{
+					Tar_Step += 1;
+				}
+			}
+			else if (Edit_Mode == 0)
+			{
+				UI_MoveUp_Cursor(&UI_target);
+			}
+		}
+		else if (UIpos == UI_sensor.Num)
+		{
+
+		}
+		else if (UIpos == UI_serial.Num)
+		{
+			UI_MoveUp_Cursor(&UI_serial);
+		}
+	}
+	/* 长按 */
+	if (Key_GetState(KEY_UP, KEY_LONG))
+	{
+		if (UIpos == UI_root.Num)
+		{
+
+		}
+		else if (UIpos == UI_start.Num)
+		{
+
+		}
+		else if (UIpos == UI_PID.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_PID.cursor == 2)
+				{
+					pPID_Motor->Kp += 0.01;
+				}
+				else if (UI_PID.cursor == 3)
+				{
+					pPID_Motor->Ki += 0.01;
+				}
+				else if (UI_PID.cursor == 4)
+				{
+					pPID_Motor->Kd += 0.01;
+				}
+				
+			}
+			else if (Edit_Mode == 0)
+			{
+
+			}
+		}
+		else if (UIpos == UI_test.Num)
+		{
 
 		}
 		else if (UIpos == UI_target.Num)
 		{
-			UI_Show(&UI_target);
-			UI_Show_test_PID_Struct(&PID_Motor1, &PID_Motor2, Tar_Step);
-		}
-		else if (UIpos == UI_sensor.Num)
-		{
-			UI_Show(&UI_sensor);
-			UI_Show_Sensor(Sensor_Data_Bit);
-		}
-		else if (UIpos == UI_serial.Num)
-		{
-			UI_Show(&UI_serial);
-			UI_Show_Serial(Serial_Out_Mode);
-		}
-		/* 按钮检测 */
-		/* 上 */
-		/* 单击 */
-		if (Key_GetState(KEY_UP, KEY_PRESS))
-		{
-			if 		(UIpos == UI_root.Num)
+			if (Edit_Mode == 1)
 			{
-				UI_MoveUp_Cursor(&UI_root);
-			}
-			else if (UIpos == UI_start.Num)
-			{
-				UI_MoveUp_Cursor(&UI_start);
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-				if (Edit_Mode == 1)
+				if (UI_target.cursor == 2)
 				{
-					if (UI_PID.cursor == 2)
-					{
-						pPID_Motor->Kp += 0.01;
-					}
-					else if (UI_PID.cursor == 3)
-					{
-						pPID_Motor->Ki += 0.01;
-					}
-					else if (UI_PID.cursor == 4)
-					{
-						pPID_Motor->Kd += 0.01;
-					}
+					PID_Motor1.Target += Tar_Step;
 				}
-				else if (Edit_Mode == 0)
+				else if (UI_target.cursor == 3)
 				{
-					UI_MoveUp_Cursor(&UI_PID);
+					PID_Motor2.Target += Tar_Step;
+				}
+				else if (UI_target.cursor == 4)
+				{
+					Tar_Step += 1;
 				}
 			}
-			else if (UIpos == UI_test.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					pPID_Motor->Target += 1;
-				}
-				else if (Edit_Mode == 0)
-				{
-					UI_MoveUp_Cursor(&UI_test);
-				}
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_target.cursor == 2)
-					{
-						PID_Motor1.Target += Tar_Step;
-					}
-					else if (UI_target.cursor == 3)
-					{
-						PID_Motor2.Target += Tar_Step;
-					}
-					else if (UI_target.cursor == 4)
-					{
-						Tar_Step += 1;
-					}
-				}
-				else if (Edit_Mode == 0)
-				{
-					UI_MoveUp_Cursor(&UI_target);
-				}
-			}
-			else if (UIpos == UI_sensor.Num)
-			{
-
-			}
-			else if (UIpos == UI_serial.Num)
-			{
-				UI_MoveUp_Cursor(&UI_serial);
-			}
-		}
-		/* 长按 */
-		if (Key_GetState(KEY_UP, KEY_LONG))
-		{
-			if (UIpos == UI_root.Num)
-			{
-
-			}
-			else if (UIpos == UI_start.Num)
-			{
-
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_PID.cursor == 2)
-					{
-						pPID_Motor->Kp += 0.01;
-					}
-					else if (UI_PID.cursor == 3)
-					{
-						pPID_Motor->Ki += 0.01;
-					}
-					else if (UI_PID.cursor == 4)
-					{
-						pPID_Motor->Kd += 0.01;
-					}
-					
-				}
-				else if (Edit_Mode == 0)
-				{
-
-				}
-			}
-			else if (UIpos == UI_test.Num)
-			{
-
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_target.cursor == 2)
-					{
-						PID_Motor1.Target += Tar_Step;
-					}
-					else if (UI_target.cursor == 3)
-					{
-						PID_Motor2.Target += Tar_Step;
-					}
-					else if (UI_target.cursor == 4)
-					{
-						Tar_Step += 1;
-					}
-				}
-				else if (Edit_Mode == 0)
-				{
-					
-				}
-			}
-		}
-		/* 二连 */
-		if (Key_GetState(KEY_UP, KEY_DOUBLE))
-		{
-
-		}
-		/* 三连 */
-		if (Key_GetState(KEY_UP, KEY_TRINPLE))
-		{
-
-		}
-		/* 下 */
-		/* 单击 */
-		if (Key_GetState(KEY_DOWN, KEY_PRESS))
-		{
-			if 		(UIpos == UI_root.Num)
-			{
-				UI_MoveDown_Cursor(&UI_root);
-			}
-			else if (UIpos == UI_start.Num)
-			{
-				UI_MoveDown_Cursor(&UI_start);
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_PID.cursor == 2)
-					{
-						pPID_Motor->Kp -= 0.01;
-					}
-					else if (UI_PID.cursor == 3)
-					{
-						pPID_Motor->Ki -= 0.01;
-					}
-					else if (UI_PID.cursor == 4)
-					{
-						pPID_Motor->Kd -= 0.01;
-					}
-				}
-				else if (Edit_Mode == 0)
-				{
-					UI_MoveDown_Cursor(&UI_PID);
-				}
-			}
-			else if (UIpos == UI_test.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					pPID_Motor->Target -= 1;
-				}
-				else if (Edit_Mode == 0)
-				{
-					UI_MoveDown_Cursor(&UI_test);
-				}
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_target.cursor == 2)
-					{
-						PID_Motor1.Target -= Tar_Step;
-					}
-					else if (UI_target.cursor == 3)
-					{
-						PID_Motor2.Target -= Tar_Step;
-					}
-					else if (UI_target.cursor == 4)
-					{
-						Tar_Step -= 1;
-					}
-				}
-				else if (Edit_Mode == 0)
-				{
-					UI_MoveDown_Cursor(&UI_target);
-				}
-			}
-			else if (UIpos == UI_serial.Num)
-			{
-				UI_MoveDown_Cursor(&UI_serial);
-			}
-		}
-		/* 长按 */
-		if (Key_GetState(KEY_DOWN, KEY_LONG))
-		{
-			if (UIpos == UI_root.Num)
-			{
-
-			}
-			else if (UIpos == UI_start.Num)
-			{
-
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-
-			}
-			else if (UIpos == UI_test.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_test.cursor == 2)
-					{
-						pPID_Motor->Target -= 1;
-					}
-				}
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					if (UI_target.cursor == 2)
-					{
-						PID_Motor1.Target -= Tar_Step;
-					}
-					else if (UI_target.cursor == 3)
-					{
-						PID_Motor2.Target -= Tar_Step;
-					}
-					else if (UI_target.cursor == 4)
-					{
-						Tar_Step -= 1;
-					}
-				}
-				else if (Edit_Mode == 0)
-				{
-					// 位置预留
-				}
-			}
-		}
-		/* 二连 */
-		if (Key_GetState(KEY_DOWN, KEY_DOUBLE))
-		{
-
-		}
-		/* 三连 */
-		if (Key_GetState(KEY_DOWN, KEY_TRINPLE))
-		{
-			
-		}
-		/* 确定 */
-		/* 单击 */
-		if (Key_GetState(KEY_CONFIRM, KEY_PRESS))
-		{
-			if 		(UIpos == UI_root.Num)
-			{
-				if (UI_root.cursor == 1)
-				{
-					UIpos = UI_start.Num;
-					UI_Reset_Cursor(&UI_root);
-					OLED_Clear();
-				}
-				else if (UI_root.cursor == 2)
-				{
-					UIpos = UI_PID.Num;
-					UI_Reset_Cursor(&UI_root);
-					OLED_Clear();
-				}
-				else if (UI_root.cursor == 3)
-				{
-					UIpos = UI_test.Num;
-					UI_Reset_Cursor(&UI_root);
-					OLED_Clear();
-				}
-				else if (UI_root.cursor == 4)
-				{
-					// 位置预留
-				}
-			}
-			else if (UIpos == UI_start.Num)
+			else if (Edit_Mode == 0)
 			{
 				
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					Edit_Mode = 0;
-					UI_Show_Edit_Mode(Edit_Mode);
-				}
-				else if (Edit_Mode == 0)
-				{
-					if (UI_PID.cursor == 1)
-					{
-						if (pPID_Motor == &PID_Motor1)
-						{
-							pPID_Motor = &PID_Motor2;
-						}
-						else
-						{
-							pPID_Motor = &PID_Motor1;
-						}
-					}
-					else
-					{
-						Edit_Mode = 1;
-						UI_Show_Edit_Mode(Edit_Mode); 
-					}
-				}
-			}
-			else if (UIpos == UI_test.Num)
-			{
-				if (UI_test.cursor == 2)
-				{
-					// 换到目标Target速度修改界面
-					UIpos = UI_target.Num;
-					UI_Reset_Cursor(&UI_test);
-					OLED_Clear();
-				}
-				else if (UI_test.cursor == 3)
-				{
-					// 换到传感器Sensor数据检测界面
-					UIpos = UI_sensor.Num;
-					UI_Reset_Cursor(&UI_test);
-					OLED_Clear();
-				}
-				else if (UI_test.cursor == 4)
-				{
-					UIpos = UI_serial.Num;
-					UI_Reset_Cursor(&UI_test);
-					OLED_Clear();
-				}
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					Edit_Mode = 0;
-					UI_Show_Edit_Mode(Edit_Mode);
-				}
-				else if (Edit_Mode == 0)
-				{
-					if (UI_target.cursor == 2)
-					{
-						Edit_Mode = 1;
-						UI_Show_Edit_Mode(Edit_Mode);
-					}
-					else if (UI_target.cursor == 3)
-					{
-						Edit_Mode = 1;
-						UI_Show_Edit_Mode(Edit_Mode);
-					}
-					else if (UI_target.cursor == 4)
-					{
-						Edit_Mode = 1;
-						UI_Show_Edit_Mode(Edit_Mode);
-					}
-				}
-			}
-			else if (UIpos == UI_sensor.Num)
-			{
-				// 没有什么可以按确定的，只能退出
-			}
-			else if (UIpos == UI_serial.Num)
-			{
-				if (UI_serial.cursor == 1)
-				{
-					Serial_Out_Mode = SERIAL_OUT_MODE_MOTOR_DATA;
-				}
-				else if (UI_serial.cursor == 2)
-				{
-					Serial_Out_Mode = SERIAL_OUT_MODE_SENSOR_DATA;
-				}
-				else if (UI_serial.cursor == 3)
-				{
-					// 位置预留
-				}
-				else if (UI_serial.cursor == 4)
-				{
-					// 位置预留
-				}
-			}
-		}
-		/* 长按 */
-		if (Key_GetState(KEY_CONFIRM, KEY_LONG))
-		{
-
-		}
-		/* 取消 */
-		if (Key_GetState(KEY_CANCEL, KEY_PRESS))
-		{
-			if 		(UIpos == UI_root.Num)
-			{
-				
-			}
-			else if (UIpos == UI_start.Num)
-			{
-				// 下面这个条件先空着，这个是要判断是不是在发车状态
-				// 如果是确认发车，这个取消是退出发车状态，而不是返回主菜单
-				if (0)
-				{
-
-				}
-				else
-				{
-					UIpos = UI_root.Num;
-					UI_Reset_Cursor(&UI_start);
-					OLED_Clear();
-				}
-			}
-			else if (UIpos == UI_PID.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					Edit_Mode = 0;
-					UI_Show_Edit_Mode(Edit_Mode);
-				}
-				else if (Edit_Mode == 0) 
-				{
-					// 返回上一级
-					UIpos = UI_root.Num;
-					UI_Reset_Cursor(&UI_PID);
-					OLED_Clear();
-				}
-			}
-			else if (UIpos == UI_test.Num)
-			{
-				UIpos = UI_root.Num;
-				UI_Reset_Cursor(&UI_test);
-				OLED_Clear();
-			}
-			else if (UIpos == UI_target.Num)
-			{
-				if (Edit_Mode == 1)
-				{
-					Edit_Mode = 0;
-					UI_Show_Edit_Mode(Edit_Mode);
-				}
-				else if (Edit_Mode == 0)
-				{
-					UIpos = UI_test.Num;
-					UI_Reset_Cursor(&UI_target);
-					OLED_Clear();
-				}
-			}
-			else if (UIpos == UI_sensor.Num)
-			{
-				UIpos = UI_test.Num;
-				UI_Reset_Cursor(&UI_sensor);
-				OLED_Clear();
-			}
-			else if (UIpos == UI_serial.Num)
-			{
-				UIpos = UI_test.Num;
-				UI_Reset_Cursor(&UI_serial);
-				OLED_Clear();
 			}
 		}
 	}
-}
+	/* 二连 */
+	if (Key_GetState(KEY_UP, KEY_DOUBLE))
+	{
 
+	}
+	/* 三连 */
+	if (Key_GetState(KEY_UP, KEY_TRINPLE))
+	{
+
+	}
+	/* 下 */
+	/* 单击 */
+	if (Key_GetState(KEY_DOWN, KEY_PRESS))
+	{
+		if 		(UIpos == UI_root.Num)
+		{
+			UI_MoveDown_Cursor(&UI_root);
+		}
+		else if (UIpos == UI_start.Num)
+		{
+			UI_MoveDown_Cursor(&UI_start);
+		}
+		else if (UIpos == UI_PID.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_PID.cursor == 2)
+				{
+					pPID_Motor->Kp -= 0.01;
+				}
+				else if (UI_PID.cursor == 3)
+				{
+					pPID_Motor->Ki -= 0.01;
+				}
+				else if (UI_PID.cursor == 4)
+				{
+					pPID_Motor->Kd -= 0.01;
+				}
+			}
+			else if (Edit_Mode == 0)
+			{
+				UI_MoveDown_Cursor(&UI_PID);
+			}
+		}
+		else if (UIpos == UI_test.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				pPID_Motor->Target -= 1;
+			}
+			else if (Edit_Mode == 0)
+			{
+				UI_MoveDown_Cursor(&UI_test);
+			}
+		}
+		else if (UIpos == UI_target.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_target.cursor == 2)
+				{
+					PID_Motor1.Target -= Tar_Step;
+				}
+				else if (UI_target.cursor == 3)
+				{
+					PID_Motor2.Target -= Tar_Step;
+				}
+				else if (UI_target.cursor == 4)
+				{
+					Tar_Step -= 1;
+				}
+			}
+			else if (Edit_Mode == 0)
+			{
+				UI_MoveDown_Cursor(&UI_target);
+			}
+		}
+		else if (UIpos == UI_serial.Num)
+		{
+			UI_MoveDown_Cursor(&UI_serial);
+		}
+	}
+	/* 长按 */
+	if (Key_GetState(KEY_DOWN, KEY_LONG))
+	{
+		if (UIpos == UI_root.Num)
+		{
+
+		}
+		else if (UIpos == UI_start.Num)
+		{
+
+		}
+		else if (UIpos == UI_PID.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_PID.cursor == 2)
+				{
+					pPID_Motor->Kp -= 0.01;
+				}
+				else if (UI_PID.cursor == 3)
+				{
+					pPID_Motor->Ki -= 0.01;
+				}
+				else if (UI_PID.cursor == 4)
+				{
+					pPID_Motor->Kd -= 0.01;
+				}
+			}
+		}
+		else if (UIpos == UI_test.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_test.cursor == 2)
+				{
+					pPID_Motor->Target -= 1;
+				}
+			}
+		}
+		else if (UIpos == UI_target.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				if (UI_target.cursor == 2)
+				{
+					PID_Motor1.Target -= Tar_Step;
+				}
+				else if (UI_target.cursor == 3)
+				{
+					PID_Motor2.Target -= Tar_Step;
+				}
+				else if (UI_target.cursor == 4)
+				{
+					Tar_Step -= 1;
+				}
+			}
+			else if (Edit_Mode == 0)
+			{
+				// 位置预留
+			}
+		}
+	}
+	/* 二连 */
+	if (Key_GetState(KEY_DOWN, KEY_DOUBLE))
+	{
+
+	}
+	/* 三连 */
+	if (Key_GetState(KEY_DOWN, KEY_TRINPLE))
+	{
+		
+	}
+	/* 确定 */
+	/* 单击 */
+	if (Key_GetState(KEY_CONFIRM, KEY_PRESS))
+	{
+		if 		(UIpos == UI_root.Num)
+		{
+			if (UI_root.cursor == 1)
+			{
+				UIpos = UI_start.Num;
+				UI_Reset_Cursor(&UI_root);
+				OLED_Clear();
+			}
+			else if (UI_root.cursor == 2)
+			{
+				UIpos = UI_PID.Num;
+				UI_Reset_Cursor(&UI_root);
+				OLED_Clear();
+			}
+			else if (UI_root.cursor == 3)
+			{
+				UIpos = UI_test.Num;
+				UI_Reset_Cursor(&UI_root);
+				OLED_Clear();
+			}
+			else if (UI_root.cursor == 4)
+			{
+				// 位置预留
+			}
+		}
+		else if (UIpos == UI_start.Num)
+		{
+			
+		}
+		else if (UIpos == UI_PID.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				Edit_Mode = 0;
+				UI_Show_Edit_Mode(Edit_Mode);
+			}
+			else if (Edit_Mode == 0)
+			{
+				if (UI_PID.cursor == 1)
+				{
+					if (pPID_Motor == &PID_Motor1)
+					{
+						pPID_Motor = &PID_Motor2;
+					}
+					else
+					{
+						pPID_Motor = &PID_Motor1;
+					}
+				}
+				else
+				{
+					Edit_Mode = 1;
+					UI_Show_Edit_Mode(Edit_Mode); 
+				}
+			}
+		}
+		else if (UIpos == UI_test.Num)
+		{
+			if (UI_test.cursor == 2)
+			{
+				// 换到目标Target速度修改界面
+				UIpos = UI_target.Num;
+				UI_Reset_Cursor(&UI_test);
+				OLED_Clear();
+			}
+			else if (UI_test.cursor == 3)
+			{
+				// 换到传感器Sensor数据检测界面
+				UIpos = UI_sensor.Num;
+				UI_Reset_Cursor(&UI_test);
+				OLED_Clear();
+			}
+			else if (UI_test.cursor == 4)
+			{
+				UIpos = UI_serial.Num;
+				UI_Reset_Cursor(&UI_test);
+				OLED_Clear();
+			}
+		}
+		else if (UIpos == UI_target.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				Edit_Mode = 0;
+				UI_Show_Edit_Mode(Edit_Mode);
+			}
+			else if (Edit_Mode == 0)
+			{
+				if (UI_target.cursor == 2)
+				{
+					Edit_Mode = 1;
+					UI_Show_Edit_Mode(Edit_Mode);
+				}
+				else if (UI_target.cursor == 3)
+				{
+					Edit_Mode = 1;
+					UI_Show_Edit_Mode(Edit_Mode);
+				}
+				else if (UI_target.cursor == 4)
+				{
+					Edit_Mode = 1;
+					UI_Show_Edit_Mode(Edit_Mode);
+				}
+			}
+		}
+		else if (UIpos == UI_sensor.Num)
+		{
+			// 没有什么可以按确定的，只能退出
+		}
+		else if (UIpos == UI_serial.Num)
+		{
+			if (UI_serial.cursor == 1)
+			{
+				Serial_Out_Mode = SERIAL_OUT_MODE_MOTOR_DATA;
+			}
+			else if (UI_serial.cursor == 2)
+			{
+				Serial_Out_Mode = SERIAL_OUT_MODE_SENSOR_DATA;
+			}
+			else if (UI_serial.cursor == 3)
+			{
+				// 位置预留
+			}
+			else if (UI_serial.cursor == 4)
+			{
+				// 位置预留
+			}
+		}
+	}
+	/* 长按 */
+	if (Key_GetState(KEY_CONFIRM, KEY_LONG))
+	{
+
+	}
+	/* 取消 */
+	if (Key_GetState(KEY_CANCEL, KEY_PRESS))
+	{
+		if 		(UIpos == UI_root.Num)
+		{
+			
+		}
+		else if (UIpos == UI_start.Num)
+		{
+			// 下面这个条件先空着，这个是要判断是不是在发车状态
+			// 如果是确认发车，这个取消是退出发车状态，而不是返回主菜单
+			if (0)
+			{
+
+			}
+			else
+			{
+				UIpos = UI_root.Num;
+				UI_Reset_Cursor(&UI_start);
+				OLED_Clear();
+			}
+		}
+		else if (UIpos == UI_PID.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				Edit_Mode = 0;
+				UI_Show_Edit_Mode(Edit_Mode);
+			}
+			else if (Edit_Mode == 0) 
+			{
+				// 返回上一级
+				UIpos = UI_root.Num;
+				UI_Reset_Cursor(&UI_PID);
+				OLED_Clear();
+			}
+		}
+		else if (UIpos == UI_test.Num)
+		{
+			UIpos = UI_root.Num;
+			UI_Reset_Cursor(&UI_test);
+			OLED_Clear();
+		}
+		else if (UIpos == UI_target.Num)
+		{
+			if (Edit_Mode == 1)
+			{
+				Edit_Mode = 0;
+				UI_Show_Edit_Mode(Edit_Mode);
+			}
+			else if (Edit_Mode == 0)
+			{
+				UIpos = UI_test.Num;
+				UI_Reset_Cursor(&UI_target);
+				OLED_Clear();
+			}
+		}
+		else if (UIpos == UI_sensor.Num)
+		{
+			UIpos = UI_test.Num;
+			UI_Reset_Cursor(&UI_sensor);
+			OLED_Clear();
+		}
+		else if (UIpos == UI_serial.Num)
+		{
+			UIpos = UI_test.Num;
+			UI_Reset_Cursor(&UI_serial);
+			OLED_Clear();
+		}
+	}
+}
 
 
 void TIM1_UP_IRQHandler(void)
